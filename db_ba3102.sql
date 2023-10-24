18de26292aeda3f5caf7541c4dfa7031c0fc03d8
Jhonathan Punzalan <144239906+Jhonzalan22@users.noreply.github.com>
2023-10-24 16:31:19 +0800

    Add files via upload

--- db_ba3102.sql
+++ db_ba3102.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Oct 23, 2023 at 05:37 AM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 24, 2023 at 07:25 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,7 +29,7 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `tbempinfo`;
 CREATE TABLE IF NOT EXISTS `tbempinfo` (
-  `empid` int(11) NOT NULL AUTO_INCREMENT,
+  `empid` int NOT NULL AUTO_INCREMENT,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `department` varchar(30) NOT NULL,
@@ -47,12 +46,124 @@ INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_studentreports`
+--
+
+DROP TABLE IF EXISTS `tbl_studentreports`;
+CREATE TABLE IF NOT EXISTS `tbl_studentreports` (
+  `StudentReportID` int NOT NULL AUTO_INCREMENT,
+  `SrCode` varchar(20) NOT NULL,
+  `LogDate` date NOT NULL,
+  `TimeIN` datetime(6) NOT NULL,
+  `TImeOut` datetime(6) NOT NULL,
+  PRIMARY KEY (`StudentReportID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_students`
+--
+
+DROP TABLE IF EXISTS `tbl_students`;
+CREATE TABLE IF NOT EXISTS `tbl_students` (
+  `SrCode` varchar(20) NOT NULL,
+  `StudentName` varchar(100) NOT NULL,
+  `StudentAdress` varchar(100) NOT NULL,
+  `StudentContactNo` varchar(100) NOT NULL,
+  `StudentDepartment` varchar(100) NOT NULL,
+  PRIMARY KEY (`SrCode`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_students`
+--
+
+INSERT INTO `tbl_students` (`SrCode`, `StudentName`, `StudentAdress`, `StudentContactNo`, `StudentDepartment`) VALUES
+('21-38207', 'Jhonathan T. Punzalan', 'San Sebastian Lipa City', '09977906459', 'CICS'),
+('21-32683', 'Jhon Matthew Evangelista', 'Pangao Lipa City', '0953556588655', 'CAS'),
+('21-67811', 'Zam Matthew Magbojos', 'Mataas Na Kahoy Batangas', '09785469874', 'CABE'),
+('21-98745', 'Jed Enrique', 'Ibaan Batangas', '09785462585', 'CIT'),
+('21-98754', 'John Victor Mamiit', 'BanayBanay Lipa City', '09785347852', 'CICS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_useradmin`
+--
+
+DROP TABLE IF EXISTS `tbl_useradmin`;
+CREATE TABLE IF NOT EXISTS `tbl_useradmin` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_useradmin`
+--
+
+INSERT INTO `tbl_useradmin` (`ID`, `Username`, `Password`) VALUES
+(1, 'Jhonathan', 'qwerty123'),
+(2, 'Matt', 'qwerty456'),
+(3, 'Zam', 'qwerty789'),
+(4, 'JV', '123456'),
+(5, 'Jed', '987654');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_visitor`
+--
+
+DROP TABLE IF EXISTS `tbl_visitor`;
+CREATE TABLE IF NOT EXISTS `tbl_visitor` (
+  `VisitorIDNo` varchar(100) NOT NULL,
+  `VisitorIDType` varchar(100) NOT NULL,
+  `VisitorName` varchar(100) NOT NULL,
+  `VisitorAdress` varchar(100) NOT NULL,
+  `VisitorContactNo` varchar(100) NOT NULL,
+  `VisitorSchool` varchar(100) NOT NULL,
+  PRIMARY KEY (`VisitorIDNo`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_visitor`
+--
+
+INSERT INTO `tbl_visitor` (`VisitorIDNo`, `VisitorIDType`, `VisitorName`, `VisitorAdress`, `VisitorContactNo`, `VisitorSchool`) VALUES
+('7895454', 'Passport', 'Peter Parker', 'Lodlod Lipa City', '09784525874', 'Lodlod Integrated National High School'),
+('985656656256565', 'School ID', 'Lawrence Magsino', 'Bolbok Lipa City', '09874546857', 'Bolbok Integrated National High School'),
+('62765264124', 'School ID', 'Matteo De Leon', 'Lumbang Lipa City', '09785458458', 'Lumbang National High School'),
+('655165165165156', 'Passport', 'Lesley Marasigan', 'Marawoy Lipa City', '09875254788', 'Inosluban Marawoy Integrated National High School'),
+('5645164984865', 'School ID', 'Heime Batumbakal', 'Calingatan Mataas na Kahoy', '09852348756', 'Bikas Integrated School');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_visitorreport`
+--
+
+DROP TABLE IF EXISTS `tbl_visitorreport`;
+CREATE TABLE IF NOT EXISTS `tbl_visitorreport` (
+  `VisitorReportID` varchar(100) NOT NULL,
+  `VisitorIDNo` varchar(100) NOT NULL,
+  `LogDate` date NOT NULL,
+  `TimeIN` datetime(6) NOT NULL,
+  `TImeOut` datetime(6) NOT NULL,
+  PRIMARY KEY (`VisitorReportID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_studinfo`
 --
 
 DROP TABLE IF EXISTS `tb_studinfo`;
 CREATE TABLE IF NOT EXISTS `tb_studinfo` (
-  `studid` int(11) NOT NULL AUTO_INCREMENT,
+  `studid` int NOT NULL AUTO_INCREMENT,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
