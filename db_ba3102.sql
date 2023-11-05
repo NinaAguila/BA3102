5405bb76ed8cffc82706c3601acaed48bce34f6e
Edbert P. Plopenio <110950380+edbertplopenio@users.noreply.github.com>
2023-11-06 07:57:54 +0800

    Add files via upload

--- db_ba3102.sql
+++ db_ba3102.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Oct 23, 2023 at 05:37 AM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 05, 2023 at 11:51 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,47 +24,150 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbempinfo`
+-- Table structure for table `equipment`
 --
 
-DROP TABLE IF EXISTS `tbempinfo`;
-CREATE TABLE IF NOT EXISTS `tbempinfo` (
-  `empid` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(25) NOT NULL,
-  `firstname` varchar(25) NOT NULL,
-  `department` varchar(30) NOT NULL,
-  PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `equipment`;
+CREATE TABLE IF NOT EXISTS `equipment` (
+  `equipmentId` int NOT NULL AUTO_INCREMENT,
+  `equipmentName` varchar(255) NOT NULL,
+  `equipmentCategoryId` int NOT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `description` text,
+  `purchaseDate` datetime DEFAULT NULL,
+  `equipmentCondition` varchar(255) DEFAULT NULL,
+  `locationId` int DEFAULT NULL,
+  `equipmentImage` blob,
+  PRIMARY KEY (`equipmentId`),
+  KEY `equipmentCategoryId` (`equipmentCategoryId`),
+  KEY `locationId` (`locationId`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tbempinfo`
+-- Dumping data for table `equipment`
 --
 
-INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'aguila', 'nina', 'cics');
+INSERT INTO `equipment` (`equipmentId`, `equipmentName`, `equipmentCategoryId`, `brand`, `quantity`, `description`, `purchaseDate`, `equipmentCondition`, `locationId`, `equipmentImage`) VALUES
+(1, 'Soccer Ball', 1, 'Brand A', 10, 'Description for Soccer Ball', '2023-10-30 12:00:00', 'New', 1, 0x2f5349412f696d616765732f736f636365725f62616c6c2e6a7067),
+(2, 'Basketball', 2, 'Brand B', 5, 'Description for Basketball', '2023-10-30 12:00:00', 'Used', 2, 0x2f5349412f696d616765732f6261736b657462616c6c2e6a7067),
+(3, 'Tennis Racket', 3, 'Brand C', 8, 'Description for Tennis Racket', '2023-10-30 12:00:00', 'Good', 3, 0x2f5349412f696d616765732f74656e6e69735f7261636b65742e6a7067),
+(4, 'Baseball Bat', 4, 'Brand D', 3, 'Description for Baseball Bat', '2023-10-30 12:00:00', 'Excellent', 4, 0x2f5349412f696d616765732f6261736562616c6c5f6261742e6a706567),
+(5, 'Volleyball', 5, 'Brand E', 12, 'Description for Volleyball', '2023-10-30 12:00:00', 'Fair', 5, 0x2f5349412f696d616765732f766f6c6c657962616c6c2e6a706567);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_studinfo`
+-- Table structure for table `equipmentcategories`
 --
 
-DROP TABLE IF EXISTS `tb_studinfo`;
-CREATE TABLE IF NOT EXISTS `tb_studinfo` (
-  `studid` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(25) NOT NULL,
-  `firstname` varchar(25) NOT NULL,
-  `course` varchar(20) NOT NULL,
-  PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `equipmentcategories`;
+CREATE TABLE IF NOT EXISTS `equipmentcategories` (
+  `categoryId` int NOT NULL AUTO_INCREMENT,
+  `categoryName` varchar(255) NOT NULL,
+  PRIMARY KEY (`categoryId`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `tb_studinfo`
+-- Dumping data for table `equipmentcategories`
 --
 
-INSERT INTO `tb_studinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
-(1, 'parker', 'peter', 'bsit'),
-(2, 'kent', 'clark', 'bscs');
+INSERT INTO `equipmentcategories` (`categoryId`, `categoryName`) VALUES
+(1, 'Soccer'),
+(2, 'Basketball'),
+(3, 'Tennis'),
+(4, 'Baseball'),
+(5, 'Volleyball');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipmentremovalrequests`
+--
+
+DROP TABLE IF EXISTS `equipmentremovalrequests`;
+CREATE TABLE IF NOT EXISTS `equipmentremovalrequests` (
+  `requestId` int NOT NULL AUTO_INCREMENT,
+  `equipmentId` int NOT NULL,
+  `requestDate` datetime DEFAULT NULL,
+  `requestedByUserId` int DEFAULT NULL,
+  `removalReason` text,
+  `quantityToRemove` int DEFAULT NULL,
+  `removalMethod` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`requestId`),
+  KEY `equipmentId` (`equipmentId`),
+  KEY `requestedByUserId` (`requestedByUserId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipmentupdates`
+--
+
+DROP TABLE IF EXISTS `equipmentupdates`;
+CREATE TABLE IF NOT EXISTS `equipmentupdates` (
+  `updateId` int NOT NULL AUTO_INCREMENT,
+  `equipmentId` int NOT NULL,
+  `updateDate` datetime DEFAULT NULL,
+  `updatedByUserId` int DEFAULT NULL,
+  `fieldUpdated` varchar(255) DEFAULT NULL,
+  `originalValue` text,
+  `newValue` text,
+  `reasonForUpdate` text,
+  PRIMARY KEY (`updateId`),
+  KEY `equipmentId` (`equipmentId`),
+  KEY `updatedByUserId` (`updatedByUserId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+CREATE TABLE IF NOT EXISTS `locations` (
+  `locationId` int NOT NULL AUTO_INCREMENT,
+  `locationName` varchar(255) NOT NULL,
+  `locationDescription` text,
+  PRIMARY KEY (`locationId`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`locationId`, `locationName`, `locationDescription`) VALUES
+(1, 'Equipment Room 1', 'Description for Equipment Room 1'),
+(2, 'Equipment Room 2', 'Description for Equipment Room 2'),
+(3, 'Storage Area A', 'Description for Storage Area A'),
+(4, 'Storage Area B', 'Description for Storage Area B'),
+(5, 'Gymnasium', 'Description for Gymnasium');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `userId` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) NOT NULL,
+  `userRole` varchar(255) NOT NULL,
+  `passwordHash` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `userName`, `userRole`, `passwordHash`) VALUES
+(1, 'Admin', 'admin', 'admin'),
+(2, 'Equipment Adder', 'equipment adder', 'adder'),
+(3, 'Equipment Remover', 'equipment remover', 'remover');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
