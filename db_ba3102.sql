77fbc02819af39b7fa3e4a9ad1fc9fd1c44197a9
John Lorenz Mayo <143990402+enzouro@users.noreply.github.com>
2023-11-21 15:48:07 +0800

    Add files via upload

--- db_ba3102.sql
+++ db_ba3102.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Oct 24, 2023 at 08:11 AM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2023 at 08:46 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,187 +24,134 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cartitems`
---
-
-DROP TABLE IF EXISTS `cartitems`;
-CREATE TABLE IF NOT EXISTS `cartitems` (
-  `CartItemID` int NOT NULL,
-  `CartID` int DEFAULT NULL,
-  `ProductID` int DEFAULT NULL,
-  `Quantity` int NOT NULL,
-  PRIMARY KEY (`CartItemID`),
-  KEY `CartID` (`CartID`),
-  KEY `ProductID` (`ProductID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `carts`
---
-
-DROP TABLE IF EXISTS `carts`;
-CREATE TABLE IF NOT EXISTS `carts` (
-  `CartID` int NOT NULL,
-  `UserID` int DEFAULT NULL,
-  PRIMARY KEY (`CartID`),
-  KEY `UserID` (`UserID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `CategoryID` int NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `Description` text,
-  PRIMARY KEY (`CategoryID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orderitems`
---
-
-DROP TABLE IF EXISTS `orderitems`;
-CREATE TABLE IF NOT EXISTS `orderitems` (
-  `OrderItemID` int NOT NULL,
-  `OrderID` int DEFAULT NULL,
-  `ProductID` int DEFAULT NULL,
-  `Quantity` int NOT NULL,
-  `Subtotal` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`OrderItemID`),
-  KEY `OrderID` (`OrderID`),
-  KEY `ProductID` (`ProductID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `OrderID` int NOT NULL,
-  `UserID` int DEFAULT NULL,
-  `OrderDate` date DEFAULT NULL,
-  `TotalAmount` decimal(10,2) NOT NULL,
-  `Status` varchar(20) NOT NULL,
-  `PaymentID` int DEFAULT NULL,
-  PRIMARY KEY (`OrderID`),
-  KEY `UserID` (`UserID`),
-  KEY `PaymentID` (`PaymentID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `payments`
---
-
-DROP TABLE IF EXISTS `payments`;
-CREATE TABLE IF NOT EXISTS `payments` (
-  `PaymentID` int NOT NULL,
-  `UserID` int DEFAULT NULL,
-  `OrderID` int DEFAULT NULL,
-  `PaymentDate` date DEFAULT NULL,
-  `PaymentMethod` varchar(50) NOT NULL,
-  `Amount` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`PaymentID`),
-  KEY `UserID` (`UserID`),
-  KEY `OrderID` (`OrderID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE IF NOT EXISTS `products` (
-  `ProductID` int NOT NULL,
-  `Name` varchar(100) NOT NULL,
-  `Description` text,
-  `Price` decimal(10,2) NOT NULL,
-  `StockQuantity` int NOT NULL,
-  `CategoryID` int DEFAULT NULL,
-  PRIMARY KEY (`ProductID`),
-  KEY `CategoryID` (`CategoryID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity_available` int(11) NOT NULL,
+  `image` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `quantity_available`, `image`) VALUES
+(1, 'Product A', 'Description for Product Aaaaaa', 25.00, 100, NULL),
+(21, 'Product A', 'Description for Product A', 25.00, 100, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reviews`
+-- Table structure for table `rgouser`
 --
 
-DROP TABLE IF EXISTS `reviews`;
-CREATE TABLE IF NOT EXISTS `reviews` (
-  `ReviewID` int NOT NULL,
-  `UserID` int DEFAULT NULL,
-  `ProductID` int DEFAULT NULL,
-  `Rating` int NOT NULL,
-  `Comment` text,
-  `Date` date DEFAULT NULL,
-  PRIMARY KEY (`ReviewID`),
-  KEY `UserID` (`UserID`),
-  KEY `ProductID` (`ProductID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `rgouser` (
+  `id` int(11) NOT NULL,
+  `empid` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','employee','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `rgouser`
+--
+
+INSERT INTO `rgouser` (`id`, `empid`, `username`, `password`, `role`) VALUES
+(1, 1, 'admin', 'RGO', 'admin'),
+(2, 2, 'employee', 'RGO', 'employee');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `tbempinfo`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `UserID` int NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `Password` varchar(100) NOT NULL,
-  `SrCode` varchar(50) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  PRIMARY KEY (`UserID`),
-  UNIQUE KEY `Email` (`Email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `tbempinfo` (
+  `empid` int(11) NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `department` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbempinfo`
+--
+
+INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
+(1, 'aguila', 'nina', 'cics'),
+(2, 'mayo', 'john', 'cics');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wishlistitems`
+-- Table structure for table `tb_studinfo`
 --
 
-DROP TABLE IF EXISTS `wishlistitems`;
-CREATE TABLE IF NOT EXISTS `wishlistitems` (
-  `WishlistItemID` int NOT NULL,
-  `WishlistID` int DEFAULT NULL,
-  `ProductID` int DEFAULT NULL,
-  PRIMARY KEY (`WishlistItemID`),
-  KEY `WishlistID` (`WishlistID`),
-  KEY `ProductID` (`ProductID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `tb_studinfo` (
+  `studid` int(11) NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_studinfo`
+--
+
+INSERT INTO `tb_studinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
+(1, 'parker', 'peter', 'bsit'),
+(2, 'kent', 'clark', 'bscs');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wishlists`
+-- Table structure for table `transactions`
 --
 
-DROP TABLE IF EXISTS `wishlists`;
-CREATE TABLE IF NOT EXISTS `wishlists` (
-  `WishlistID` int NOT NULL,
-  `UserID` int DEFAULT NULL,
-  PRIMARY KEY (`WishlistID`),
-  KEY `UserID` (`UserID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `transactions` (
+  `transaction_id` int(11) NOT NULL,
+  `employee_id` int(11) DEFAULT NULL,
+  `transaction_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total_cost` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transaction_id`, `employee_id`, `transaction_date`, `total_cost`) VALUES
+(1, 2, '2023-11-19 02:45:59', 75.00),
+(2, 3, '2023-11-19 02:45:59', 100.50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_details`
+--
+
+CREATE TABLE `transaction_details` (
+  `transaction_detail_id` int(11) NOT NULL,
+  `transaction_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity_sold` int(11) NOT NULL,
+  `unit_price` decimal(10,2) NOT NULL,
+  `subtotal` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaction_details`
+--
+
+INSERT INTO `transaction_details` (`transaction_detail_id`, `transaction_id`, `product_id`, `quantity_sold`, `unit_price`, `subtotal`) VALUES
+(1, 1, 1, 2, 25.00, 50.00),
+(2, 1, 2, 1, 35.50, 35.50),
+(3, 2, 3, 3, 50.25, 150.75);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
