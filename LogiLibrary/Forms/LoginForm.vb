2c2885909e1e73a3ee85c9abb469bc2d816405f2
Jhon Matthew Evangelista <143513397+MateoDuke21@users.noreply.github.com>
2023-11-29 22:58:52 +0800

    System Files Upload

---
+++ LogiLibrary/Forms/LoginForm.vb
@@ -0,0 +1,56 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class LoginForm
+    Dim username As String
+    Dim password As String
+    Private Sub btnlogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        Try
+            Using strcon As MySqlConnection = New MySqlConnection("server=localhost;user id=root;database=db_ba3102")
+                strcon.Open()
+
+                Dim found As Boolean = False
+
+                Using cmd As MySqlCommand = New MySqlCommand("SELECT * FROM `tbl_useradmin` WHERE `Username`=@Username AND `Password`=@Password", strcon)
+                    cmd.Parameters.AddWithValue("@Username", txtUser.Text)
+                    cmd.Parameters.AddWithValue("@Password", txtPass.Text)
+
+                    Using dr As MySqlDataReader = cmd.ExecuteReader
+                        If dr.Read() Then
+                            found = True
+                            username = dr("Username").ToString()
+                            password = dr("Password").ToString()
+                        End If
+                    End Using
+                End Using
+
+                If found Then
+                    If Not (String.Equals(txtUser.Text, username, StringComparison.Ordinal) AndAlso
+                            String.Equals(txtPass.Text, password, StringComparison.Ordinal)) Then
+                        MsgBox("Wrong Username or Password!", vbExclamation, "Invalid Input")
+                    Else
+                        Me.Hide()
+                        strcon.Close()
+                        MainDashboard.Show()
+                    End If
+                    strcon.Close()
+                Else
+                    MsgBox("Please Enter Correct Username & Password!", vbExclamation, "Invalid Input")
+                End If
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            strcon.Close()
+        End Try
+        strcon.Close()
+        txtPass.Clear()
+        txtUser.Clear()
+    End Sub
+
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+        If MsgBox("Are you sure you want to exit?", vbInformation + vbYesNo, "LogiLibrary") = vbYes Then
+            End
+        Else
+            Return
+        End If
+    End Sub
+End Class
---
+++ LogiLibrary/Forms/MainDashboard.vb
@@ -0,0 +1,75 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class MainDashboard
+    Private WithEvents refreshTimer As New Timer()
+
+    Private Sub btnAccess_Click(sender As Object, e As EventArgs) Handles btnAccess.Click
+        Me.Hide()
+        StudentVistorAccess.Show()
+    End Sub
+
+    Private Sub btnVisitorRegistration_Click(sender As Object, e As EventArgs) Handles btnVisitorRegistration.Click
+        Me.Hide()
+        VisitorRegistration.Show()
+    End Sub
+
+    Private Sub btnReports_Click(sender As Object, e As EventArgs) Handles btnReports.Click
+        Me.Hide()
+        Reports.Show()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lbl_time.Text = Date.Now.ToString("hh:mm:ss tt")
+        lbl_date.Text = Date.Now.ToString("dd-MMMM-yyyy dddd")
+        Load_NoOfStudentsDept()
+        Load_NoOfVisitors()
+    End Sub
+
+    Private Sub MainDashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set the timer interval to refresh every 60 seconds (adjust as needed)
+        refreshTimer.Interval = 60000
+        refreshTimer.Start()
+
+        ' Initial load
+        Load_NoOfStudentsDept()
+        Load_NoOfVisitors()
+    End Sub
+
+    Sub Load_NoOfStudentsDept()
+        Try
+            Using strcon As MySqlConnection = New MySqlConnection("server=localhost;user id=root;database=db_ba3102")
+                strcon.Open()
+                Dim cmd As MySqlCommand = New MySqlCommand("Select COUNT(tbl_studentlogs.SrCode) FROM tbl_studentlogs INNER JOIN tbl_student On tbl_studentlogs.SrCode = tbl_student.SrCode WHERE tbl_student.StudentDepartment = 'CICS' AND tbl_studentlogs.LogDate ='" & Date.Now.ToString("dd-MMMM-yyyy dddd") & "'", strcon)
+                lbl_CICS.Text = cmd.ExecuteScalar.ToString + " students"
+                cmd = New MySqlCommand("SELECT COUNT(tbl_studentlogs.SrCode) FROM tbl_studentlogs INNER JOIN tbl_student ON tbl_studentlogs.SrCode = tbl_student.SrCode WHERE tbl_student.StudentDepartment = 'CTE' AND tbl_studentlogs.LogDate ='" & Date.Now.ToString("dd-MMMM-yyyy dddd") & "'", strcon)
+                lbl_CTE.Text = cmd.ExecuteScalar.ToString + " students"
+                cmd = New MySqlCommand("SELECT COUNT(tbl_studentlogs.SrCode) FROM tbl_studentlogs INNER JOIN tbl_student ON tbl_studentlogs.SrCode = tbl_student.SrCode WHERE tbl_student.StudentDepartment = 'CIT' AND tbl_studentlogs.LogDate ='" & Date.Now.ToString("dd-MMMM-yyyy dddd") & "'", strcon)
+                lbl_CIT.Text = cmd.ExecuteScalar.ToString + " students"
+                cmd = New MySqlCommand("SELECT COUNT(tbl_studentlogs.SrCode) FROM tbl_studentlogs INNER JOIN tbl_student ON tbl_studentlogs.SrCode = tbl_student.SrCode WHERE tbl_student.StudentDepartment = 'CAS' AND tbl_studentlogs.LogDate ='" & Date.Now.ToString("dd-MMMM-yyyy dddd") & "'", strcon)
+                lbl_CAS.Text = cmd.ExecuteScalar.ToString + " students"
+                cmd = New MySqlCommand("SELECT COUNT(tbl_studentlogs.SrCode) FROM tbl_studentlogs INNER JOIN tbl_student ON tbl_studentlogs.SrCode = tbl_student.SrCode WHERE tbl_student.StudentDepartment = 'CABE' AND tbl_studentlogs.LogDate ='" & Date.Now.ToString("dd-MMMM-yyyy dddd") & "'", strcon)
+                lbl_CABE.Text = cmd.ExecuteScalar.ToString + " students"
+
+            End Using ' Automatically closes the connection when leaving the block
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Sub Load_NoOfVisitors()
+        Try
+            Using strcon As MySqlConnection = New MySqlConnection("server=localhost;user id=root;database=db_ba3102")
+                strcon.Open()
+                Dim cmd As MySqlCommand = New MySqlCommand("Select COUNT(tbl_visitorlogs.VisitorName) FROM tbl_visitorlogs INNER JOIN tbl_visitor On tbl_visitorlogs.VisitorName = tbl_visitor.VisitorName WHERE tbl_visitorlogs.LogDate ='" & Date.Now.ToString("dd-MMMM-yyyy dddd") & "'", strcon)
+                lbl_Visitor.Text = cmd.ExecuteScalar.ToString + " visitors"
+            End Using ' Automatically closes the connection when leaving the block
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+End Class
